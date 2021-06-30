053eea922ad1d0dd51bb90f27f1e009f314e02fd
Dylan <dylantellei@gmail.com>
2021-06-30 16:20:20 +0900

    Navigation and HomePages

--- belauMakit/src/components/Authentication.js
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
-
-import auth from '@react-native-firebase/auth';
-import firestore from '../config/FirebaseConfig';
-import firebase from 'firebase';
-
